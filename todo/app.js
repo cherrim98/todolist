4c8276007c4282b1237bb90668ad480bf2719ab7
Aaron <cherrim98@gmail.com>
2016-05-20 09:22:25 -0400

    Add files via upload

---
+++ todo/app.js
@@ -0,0 +1,37 @@
+angular.module("todoApp", [])
+.controller("TodoListController", function($scope) {
+  var todoList = this;
+  todoList.todoItems = [
+    { name: "*placeholder*", done: false },
+    { name: "*placeholder*", done: false },
+    { name: "*placeholder*", done: false },
+    { name: "*placeholder*", done: false }
+  ]
+
+  todoList.addTodo = function() {
+    todoList.todoItems.push(
+      { name: todoList.todoText }
+    )
+    todoList.todoText = "";
+  };
+
+  todoList.remove = function() {
+    var oldTodos = todoList.todoItems;
+    todoList.todoItems = [];
+    angular.forEach(oldTodos, function(todo) {
+      if(!todo.done) todoList.todoItems.push(todo);
+    })
+    todoList.todoItems
+  };
+
+  todoList.remaining = function() {
+    var notCompletedCount = 0;
+    angular.forEach(todoList.todoItems, function(todo) {
+      notCompletedCount += todo.done ? 0 : 1;
+    })
+
+    return notCompletedCount
+  }
+
+
+}); // end TodoListController
